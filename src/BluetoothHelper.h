4ec50f5cf05c2b364cef9fab55fc8847fbc09cbb
Nick Tong <nicktong.001@gmail.com>
2022-04-24 22:13:58 -0400

    Added buffer for bluetooth input

--- src/BluetoothHelper.h
+++ src/BluetoothHelper.h
@@ -1,6 +1,14 @@
-#include "pch.h"
+#pragma once
+
+struct BLEDevice
+{
+	uint8_t* dataBuffer;
+	Platform::Guid serviceGuid;
+	Platform::Guid characteristicGuid;
+};
 
 bool StartWatcher();
+
 concurrency::task<bool> Initialize(Windows::Devices::Bluetooth::BluetoothLEDevice^ bleDevice, 
 	Platform::Guid serviceGuid, Platform::Guid characteristicGuid);
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "BluetoothHelper.h"
 
+constexpr auto BUFFER_LENGTH = 128;
+
 struct Vector3
 {
 	float X;
@@ -38,8 +40,68 @@ bool TryProcessPacket(UINT8* bytes)
 	return true;
 }
 
+uint8_t buffer[BUFFER_LENGTH];
+unsigned int readIndex;
+unsigned int writeIndex;
+
+void WriteBuffer(uint8_t byte)
+{
+	buffer[writeIndex] = byte;
+	writeIndex++;
+	if (writeIndex >= BUFFER_LENGTH)
+	{
+		writeIndex = 0;
+	}
+}
+
+uint8_t ReadBuffer()
+{
+	uint8_t byte = buffer[readIndex];
+	readIndex++;
+	if (readIndex >= BUFFER_LENGTH)
+	{
+		readIndex = 0;
+	}
+
+	return byte;
+}
+
+int BufferCount()
+{
+	int difference = writeIndex - readIndex;
+	if (difference < 0) difference += BUFFER_LENGTH;
+	return difference;
+}
+
+void ProcessCharacteristicValue(Windows::Storage::Streams::IBuffer^ characteristicValue)
+{
+	auto reader = Windows::Storage::Streams::DataReader::FromBuffer(characteristicValue);
+	for (unsigned int i = 0; i < characteristicValue->Length; i++)
+	{
+		WriteBuffer(reader->ReadByte());
+	}
+}
+
 int main(Platform::Array<Platform::String^>^ args)
 {
+	WriteBuffer(10);
+	ReadBuffer();
+	WriteBuffer(10);
+	ReadBuffer();
+	WriteBuffer(10);
+	WriteBuffer(10);
+	WriteBuffer(10);
+	WriteBuffer(10);
+
+	std::cout << "Read index: " << readIndex << std::endl;
+	std::cout << "Write index: " << writeIndex << std::endl;
+
+	std::cout << "Buffer length: " << BufferCount() << std::endl;
+	std::cout << (int)ReadBuffer() << std::endl;
+	std::cout << "Buffer length: " << BufferCount() << std::endl;
+
+	return 0;
+
 	HRESULT hresult = RoInitialize(RO_INIT_MULTITHREADED);
 
 	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
